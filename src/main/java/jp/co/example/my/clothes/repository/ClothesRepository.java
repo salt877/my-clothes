9fe4985be60baafb4e71b0376260c9b35e56b68b
nosemasashi <ma.nose9413@gmail.com>
2020-07-06 14:22:33 +0900

    Clothesリポジトリに統計表示用メソッドを追加しました。

--- src/main/java/jp/co/example/my/clothes/repository/ClothesRepository.java
+++ src/main/java/jp/co/example/my/clothes/repository/ClothesRepository.java
@@ -1,9 +1,12 @@
 package jp.co.example.my.clothes.repository;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
@@ -34,6 +37,64 @@ public class ClothesRepository {
 		SqlParameterSource param = new BeanPropertySqlParameterSource(clothes);
 		template.update(sql, param);
 
+	}
+
+	/**
+	 * ユーザーIDで検索.
+	 * 
+	 * @param userId ユーザーID
+	 * @return clothesオブジェクトが入ったリスト（０件の場合nullを返します。）
+	 */
+	public List<Clothes> findByUserId(Integer userId) {
+		String sql = "SELECT user_id, image_path, price, category_id, brand_id, color_id, size_id, season, perchase_date, comment FROM clothes WHERE user_id = :userId";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("userId", userId);
+		List<Clothes> clothesList = template.query(sql, param, CLOTHES_ROW_MAPPER);
+
+		if (clothesList.size() == 0) {
+			return null;
+		}
+
+		return clothesList;
+
+	}
+
+	/**
+	 * ユーザーIDとカテゴリーIDから服情報を検索.
+	 * 
+	 * @param userId     ユーザーID
+	 * @param categoryId カテゴリーID
+	 * @return clothesオブジェクトが入ったリスト（０件の場合nullを返します。）
+	 */
+	public List<Clothes> findByUserIdAndCategoryId(Integer userId, Integer categoryId) {
+		String sql = "SELECT user_id, image_path, price, category_id, brand_id, color_id, size_id, season, perchase_date, comment FROM clothes WHERE user_id = :user_id AND category_id = :categoryId";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("userId", userId).addValue("categoryId",
+				categoryId);
+		List<Clothes> clothesListByUserIdAndCategoryId = template.query(sql, param, CLOTHES_ROW_MAPPER);
+
+		if (clothesListByUserIdAndCategoryId.size() == 0) {
+			return null;
+		}
+
+		return clothesListByUserIdAndCategoryId;
+	}
+
+	/**
+	 * ユーザーIDとブランドIDから服情報を検索.
+	 * 
+	 * @param userId  ユーザーID
+	 * @param brandId ブランドID
+	 * @return clothesオブジェクトが入ったリスト（０件の場合nullを返します。）
+	 */
+	public List<Clothes> findByUserIdAndBrandId(Integer userId, Integer brandId) {
+		String sql = "SELECT user_id, image_path, price, category_id, brand_id, color_id, size_id, season, perchase_date, comment FROM clothes WHERE user_id = :user_id AND category_id = :brandId";
+		SqlParameterSource param = new MapSqlParameterSource().addValue("userId", userId).addValue("brandId", brandId);
+		List<Clothes> clothesListByUserIdAndBrandId = template.query(sql, param, CLOTHES_ROW_MAPPER);
+
+		if (clothesListByUserIdAndBrandId.size() == 0) {
+			return null;
+		}
+
+		return clothesListByUserIdAndBrandId;
 	}
 
 }
