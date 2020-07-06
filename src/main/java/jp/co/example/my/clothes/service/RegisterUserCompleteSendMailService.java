3d500aeddfa85be64ae8a7aa1ac33659fd550502
tanimorimizuki <mizuki.tanimori@rakus-partners.co.jp>
2020-07-06 15:23:06 +0900

    例外処理を追記しました。

--- src/main/java/jp/co/example/my/clothes/service/RegisterUserCompleteSendMailService.java
+++ src/main/java/jp/co/example/my/clothes/service/RegisterUserCompleteSendMailService.java
@@ -18,36 +18,36 @@ import jp.co.example.my.clothes.repository.UserRepository;
 @Service
 @Transactional
 public class RegisterUserCompleteSendMailService {
-	
+
 	@Autowired
 	private MailSender sender;
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	/**
 	 * ユーザ登録完了後にメールを送信するメソッド.
 	 */
 	public void sendMail() {
 		// メール情報を詰めるオブジェクト
 		SimpleMailMessage msg = new SimpleMailMessage();
-		
+
 		// 送信元アドレス
 		msg.setFrom("my.clothes0702@gmail.com");
-		
+
 		// ユーザ情報から送信先のメールアドレスをセット
 		User user = new User();
 		msg.setTo(user.getEmail());
-		
+
 		// 件名
 		msg.setSubject("【MYQLO】ユーザ登録完了のお知らせ（自動送信メール）");
-	
+
 		// 本文を作成
 		StringBuilder text = new StringBuilder();
-		
+
 		// お名前
 		text.append("\n" + "新規ユーザ様" + "\n\n");
-		
+
 		// 本文
 		text.append("お世話になっております。\n");
 		text.append("MYQLO運営事務局です。この度は本サイトにご登録いただきありがとうございます。\n");
@@ -58,7 +58,7 @@ public class RegisterUserCompleteSendMailService {
 		text.append("パスワード：" + user.getPassword() + "\n\n");
 		text.append("引き続き、MYQLOをお楽しみください。\n");
 		text.append("今後ともよろしくお願い申し上げます。\n");
-		
+
 		// フッター部分
 		text.append("──────────────────────────────────────────\n");
 		text.append("MYQLO" + "\n");
@@ -68,11 +68,18 @@ public class RegisterUserCompleteSendMailService {
 		text.append("営業時間：平日 oo:oo 〜 oo:oo  " + "\n");
 		text.append("お問合せメールへの返信は2営業日以内にいたします。\n");
 		text.append("──────────────────────────────────────────");
-		
-		
-		
-		
-	
+
+		// 上記で作成した本文をセット
+		msg.setText(text.toString());
+
+		try {
+			// 送信処理
+			this.sender.send(msg);
+		} catch (Exception e) {
+			// 例外処理が必要
+			e.printStackTrace();
+		}
+
 	}
 
 }
