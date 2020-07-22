cc955586f17036834f2f448ccdfba3c04425e32f
tanimorimizuki <mizuki.tanimori@rakus-partners.co.jp>
2020-07-22 12:25:49 +0900

    サービスクラスに詳細情報を取得するメソッドを追記しました。

--- src/main/java/jp/co/example/my/clothes/service/ShowClothesDetailService.java
+++ src/main/java/jp/co/example/my/clothes/service/ShowClothesDetailService.java
@@ -1,5 +1,24 @@
 package jp.co.example.my.clothes.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import jp.co.example.my.clothes.domain.Clothes;
+import jp.co.example.my.clothes.repository.ClothesRepository;
+
+@Service
 public class ShowClothesDetailService {
 
+	@Autowired
+	private ClothesRepository clothesRepository;
+	
+	/**
+	 * アイテムIDで詳細情報を取得します.
+	 * 
+	 * @param id アイテムID
+	 * @return 1件のアイテム情報
+	 */
+	public Clothes showClothesDetail(Integer id) {
+		return clothesRepository.findById(id);
+	}
 }
