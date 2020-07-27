9c6570d9e5f59e8118976c0932d84fc6e8372ffd
tanimorimizuki <mizuki.tanimori@rakus-partners.co.jp>
2020-07-27 11:10:30 +0900

    アイテムIDでタグ一覧を取得するメソッドを追記しました。

--- src/main/java/jp/co/example/my/clothes/service/ShowClothesDetailService.java
+++ src/main/java/jp/co/example/my/clothes/service/ShowClothesDetailService.java
@@ -1,16 +1,23 @@
 package jp.co.example.my.clothes.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import jp.co.example.my.clothes.domain.Clothes;
+import jp.co.example.my.clothes.domain.Tag;
 import jp.co.example.my.clothes.repository.ClothesRepository;
+import jp.co.example.my.clothes.repository.TagRepository;
 
 @Service
 public class ShowClothesDetailService {
 
 	@Autowired
 	private ClothesRepository clothesRepository;
+	
+	@Autowired
+	private TagRepository tagRepository;
 	
 	/**
 	 * アイテムIDで詳細情報を取得します.
@@ -20,5 +27,15 @@ public class ShowClothesDetailService {
 	 */
 	public Clothes showClothesDetail(Integer id) {
 		return clothesRepository.findById(id);
+	}
+	
+	/**
+	 * アイテムIDでタグ一覧を取得します.
+	 * 
+	 * @param id アイテムID
+	 * @return タグ一覧
+	 */
+	public List<Tag> showTagList(Integer id){
+		return tagRepository.findById(id);
 	}
 }
