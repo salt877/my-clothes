88a18d7659d9458f39d515c0a886cd4e4e4e7d54
tanimorimizuki <mizuki.tanimori@rakus-partners.co.jp>
2020-07-27 11:34:22 +0900

    Autowiredを追記しました。

--- src/main/java/jp/co/example/my/clothes/controller/showClothesDetailController.java
+++ src/main/java/jp/co/example/my/clothes/controller/showClothesDetailController.java
@@ -1,5 +1,17 @@
 package jp.co.example.my.clothes.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jp.co.example.my.clothes.service.ShowClothesDetailService;
+
+@Controller
+@RequestMapping("/showDetail")
 public class showClothesDetailController {
 
+	@Autowired
+	private ShowClothesDetailService showClothesDetailService;
+	
+	
 }
