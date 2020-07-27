be1a116fd04f578497998537b1e2c4aebe8cadcd
tanimorimizuki <mizuki.tanimori@rakus-partners.co.jp>
2020-07-27 13:11:38 +0900

    アイテムの詳細画面を表示するメソッドを追記しました。

--- src/main/java/jp/co/example/my/clothes/controller/showClothesDetailController.java
+++ src/main/java/jp/co/example/my/clothes/controller/showClothesDetailController.java
@@ -1,9 +1,14 @@
 package jp.co.example.my.clothes.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jp.co.example.my.clothes.domain.Clothes;
+import jp.co.example.my.clothes.domain.Tag;
 import jp.co.example.my.clothes.service.ShowClothesDetailService;
 
 @Controller
@@ -12,6 +17,22 @@ public class showClothesDetailController {
 
 	@Autowired
 	private ShowClothesDetailService showClothesDetailService;
+	
+	/**
+	 * アイテムの詳細画面を表示するメソッドです.
+	 * 
+	 * @param model リクエストスコープ
+	 * @param id アイテムID
+	 * @return 詳細画面遷移
+	 */
+	@RequestMapping("")
+	public String toClothesDetail(Model model, Integer id) {
+		Clothes clothes = showClothesDetailService.showClothesDetail(id);
+		List<Tag>tagList = showClothesDetailService.showTagList(id);
+		model.addAttribute("tagList", tagList);
+		model.addAttribute("clothes", clothes);
+		return "edit_clothes";
+	}
 	
 	
 }
