d93271c16a7fdbedf72343ee9ea00a75dc8bcdf5
shiodarina <rina.shioda@rakus-partners.co.jp>
2020-07-28 16:14:31 +0900

    不要なクラスを削除しました。

--- src/main/java/jp/co/example/my/clothes/IUserService.java
+++
@@ -1,9 +0,0 @@
-package jp.co.example.my.clothes;
-
-import jp.co.example.my.clothes.domain.User;
-
-public interface IUserService {
-	
-	 void createPasswordResetTokenForUser(User user, String token);
-
-}
