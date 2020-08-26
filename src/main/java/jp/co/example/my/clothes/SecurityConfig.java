fdc9c05e0a0535588fb8dde71f16482fcc5f65c5
shiodarina <rina.shioda@rakus-partners.co.jp>
2020-08-26 15:00:14 +0900

    SpringSecurityの設定変更

--- src/main/java/jp/co/example/my/clothes/SecurityConfig.java
+++ src/main/java/jp/co/example/my/clothes/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 
 		http.authorizeRequests() // 認可に関する設定
-				.antMatchers("/showLogin","/showRegisterUser","/forgotPassword","/passwordInputPage").permitAll()
+				.antMatchers("myqlo.herokuapp.com/showLogin","myqlo.herokuapp.com/showRegisterUser","myqlo.herokuapp.com/forgotPassword","myqlo.herokuapp.com/passwordInputPage").permitAll()
 				.anyRequest().authenticated(); 
 
 		http.formLogin() // ログインに関する設定
